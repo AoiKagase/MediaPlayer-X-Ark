0fe6193cd6c035df091f1df5a80ce53ae797e5f3
Aoi Kagase <datumoujidai@gmail.com>
2022-04-10 23:27:31 +0900

    Effector

---
+++ Engine/Effectors/FmodEffectorBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPlayer_X_Ark.Engine
+{
+    public class FmodEffectorBase
+    {
+        protected FMOD.System _system;
+        protected FMOD.DSP _dsp;
+        protected FMOD.ChannelGroup _channelGroup;
+
+        public FmodEffectorBase(FMOD.System system, FMOD.DSP_TYPE dspType)
+        {
+            if (system.createDSPByType(dspType, out _dsp) == FMOD.RESULT.OK)
+            {
+                _system = system;
+                _system.getMasterChannelGroup(out _channelGroup);
+                _channelGroup.addDSP(0, _dsp);
+            }
+        }
+
+        ~FmodEffectorBase()
+        {
+            if (_dsp.hasHandle())
+                _channelGroup.removeDSP(_dsp);
+
+            _dsp.release();
+            //_channelGroup.release();
+        }
+
+        public FMOD.RESULT Switch(bool sw)
+        {
+            FMOD.RESULT result = FMOD.RESULT.OK;
+            bool active;
+            _dsp.getActive(out active);
+            if (active)
+                if (sw == false)
+                    _dsp.setActive(false);
+            else
+                if (sw == true)
+                    _dsp.setActive(true);
+
+            return result;
+        }
+
+        public FMOD.RESULT SetParameter(int type, float value)
+        {
+            return _dsp.setParameterFloat(type, value);
+        }
+    }
+}
---
+++ Engine/Effectors/FmodEffectorLowpass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPlayer_X_Ark.Engine
+{
+	public class FmodEffectorLowpass : FmodEffectorBase
+	{
+		private float _cutoff;
+		private float _resonance;
+
+		/// <summary>
+		/// Lowpass cutoff frequency.
+		/// Type: float Units: Hertz Range: [1, 22000] Default: 5000
+		/// </summary>
+		public float CutOff
+		{
+			get
+			{
+				return _cutoff;
+			}
+			set
+			{
+				if (_cutoff != value)
+				{
+					if (value < 1.0F)
+						value = 1.0F;
+					if (value > 22000.0F)
+						value = 22000.0F;
+
+					_cutoff = value;
+					SetParameter((int)FMOD.DSP_LOWPASS.CUTOFF, value);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Lowpass resonance Q value.
+		/// Type: float Range: [0, 10] Default: 1
+		/// </summary>
+		public float Resonance
+		{
+			get
+			{
+				return _resonance;
+			}
+			set
+			{
+				if (value != _resonance)
+				{
+					if (value < 0.0F)
+						value = 0.0F;
+					if (value > 10.0F)
+						value = 10.0F;
+
+					_resonance = value;
+					SetParameter((int)FMOD.DSP_LOWPASS.RESONANCE, value);
+				}
+			}
+		}
+
+		/// <summary>
+		/// CREATE DSP FOR LOWPASS FILTER
+		/// </summary>
+		/// <param name="system"></param>
+		public FmodEffectorLowpass(FMOD.System system) : base(system, FMOD.DSP_TYPE.LOWPASS)
+		{
+		}
+	}
+}
