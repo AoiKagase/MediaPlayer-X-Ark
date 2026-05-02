0aad5d7c684cd27a9c7266ca054feef37850d9e5
Aoi Kagase <datumoujidai@gmail.com>
2026-05-02 19:54:35 +0900

    エラートーストのマウス入力透過対応

--- Forms/ErrorToastForm.cs
+++ Forms/ErrorToastForm.cs
@@ -10,15 +10,18 @@ namespace MediaPlayer_X_Ark.Forms
 		private double _displaySeconds = 0;
 		private bool _fading = false;
 		private const double FadeStep = 0.06;
+		private const int WS_EX_NOACTIVATE = 0x08000000;
+		private const int WS_EX_TOOLWINDOW = 0x00000080;
+		private const int WS_EX_TRANSPARENT = 0x00000020;
+		private const int WM_NCHITTEST = 0x0084;
+		private static readonly IntPtr HTTRANSPARENT = new IntPtr(-1);
 
 		protected override CreateParams CreateParams
 		{
 			get
 			{
-				const int WS_EX_NOACTIVATE = 0x08000000;
-				const int WS_EX_TOOLWINDOW = 0x00000080;
 				var cp = base.CreateParams;
-				cp.ExStyle |= WS_EX_NOACTIVATE | WS_EX_TOOLWINDOW;
+				cp.ExStyle |= WS_EX_NOACTIVATE | WS_EX_TOOLWINDOW | WS_EX_TRANSPARENT;
 				return cp;
 			}
 		}
@@ -40,7 +43,7 @@ namespace MediaPlayer_X_Ark.Forms
 			Size = new Size(300, 64);
 			StartPosition = FormStartPosition.Manual;
 
-			var icon = new Label
+			var icon = new HitTestTransparentLabel
 			{
 				Text = "⚠",
 				ForeColor = Color.Orange,
@@ -51,7 +54,7 @@ namespace MediaPlayer_X_Ark.Forms
 				TextAlign = ContentAlignment.MiddleCenter,
 			};
 
-			var messageLabel = new Label
+			var messageLabel = new HitTestTransparentLabel
 			{
 				Text = message,
 				ForeColor = Color.White,
@@ -99,6 +102,31 @@ namespace MediaPlayer_X_Ark.Forms
 					_fadeTimer.Stop();
 					Close();
 				}
+			}
+		}
+
+		protected override void WndProc(ref Message m)
+		{
+			if (m.Msg == WM_NCHITTEST)
+			{
+				m.Result = HTTRANSPARENT;
+				return;
+			}
+
+			base.WndProc(ref m);
+		}
+
+		private sealed class HitTestTransparentLabel : Label
+		{
+			protected override void WndProc(ref Message m)
+			{
+				if (m.Msg == WM_NCHITTEST)
+				{
+					m.Result = HTTRANSPARENT;
+					return;
+				}
+
+				base.WndProc(ref m);
 			}
 		}
 
